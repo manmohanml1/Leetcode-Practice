980ede0c37d07f89b4a65ef9118319c3ae033796
manmohanml1 <manmohanlonawat@gmail.com>
2025-03-03 20:42:06 -0500

    Added GitHub Actions for testing and a couple of testcases for two-sum problem

--- tests/easy/two-sum.test.ts
+++ tests/easy/two-sum.test.ts
@@ -1,5 +1,12 @@
 import twoSum from "../../src/easy/two-sum";
 
-test("twoSum example test", () => {
+test("twoSum example test1", () => {
   expect(twoSum([2, 7, 11, 15], 9)).toEqual([0, 1]);
+});
+
+test("twoSum example test2", () => {
+  expect(twoSum([3, 2, 4], 6)).toEqual([1, 2]);
+});
+test("twoSum example test3", () => {
+  expect(twoSum([3, 3], 6)).toEqual([0, 1]);
 });
