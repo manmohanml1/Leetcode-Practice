bd837aff1c46284e3ee14f8e283644532c3f8ef7
manmohanml1 <manmohanlonawat@gmail.com>
2025-04-06 23:33:50 -0400

    updated test cases

--- python/tests/ML/easy/test_LC_217_contains_duplicate.py
+++ python/tests/ML/easy/test_LC_217_contains_duplicate.py
@@ -4,5 +4,5 @@ class TestContainsDuplicate(unittest.TestCase):
         
     def test_contains_duplicate(self):
         self.assertTrue(contains_duplicate2([1,2,3,1])) 
-        self.assertFalse(contains_duplicate2([1,2,3,1]))
+        self.assertFalse(contains_duplicate2([1,2,3,4]))
         self.assertTrue(contains_duplicate2([1,1,1,3,3,4,3,2,4,2]))
